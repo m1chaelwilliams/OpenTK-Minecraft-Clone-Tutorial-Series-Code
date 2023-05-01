6c1e85fd85d4b8e6643d9bd44a9b9e6e9262f7f2
m1chaelwilliams <108592368+m1chaelwilliams@users.noreply.github.com>
2023-05-01 14:46:29 -0500

    Add files via upload

---
+++ ep 2/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace openTK_Minecraft_Clone_Tutorial_Series
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(Game game = new Game(500,500))
+            {
+                game.Run();
+            }
+        }
+    }
+
+}
