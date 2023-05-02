e734f60514bf5428de14ed86cea18a4ebed93971
m1chaelwilliams <108592368+m1chaelwilliams@users.noreply.github.com>
2023-05-02 16:34:42 -0500

    Add files via upload

---
+++ ep 3/Game.cs
@@ -0,0 +1,140 @@
+﻿using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Windowing.Common;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Minecraft_Clone_Tutorial_Series_videoproj
+{
+    // Game class that inherets from the Game Window Class
+    internal class Game : GameWindow
+    {
+
+        float[] vertices =
+        {
+            0f, 0.5f, 0f, // top vertex
+            -0.5f, -0.5f, 0f, // bottom left
+            0.5f, -0.5f, 0f // bottom right
+        };
+
+        // Render Pipeline vars
+        int vao;
+        int shaderProgram;
+        int vbo;
+
+        // width and height of screen
+        int width, height;
+        // Constructor that sets the width, height, and calls the base constructor (GameWindow's Constructor) with default args
+        public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.width = width;
+            this.height = height;
+
+            // center window
+            CenterWindow(new Vector2i(width, height));
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0,0, e.Width, e.Height);
+            this.width = e.Width;
+            this.height = e.Height;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            vao = GL.GenVertexArray();
+
+            vbo = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length*sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            // bind the vao
+            GL.BindVertexArray(vao);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexArrayAttrib(vao, 0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0); // unbinding the vbo
+            GL.BindVertexArray(0);
+
+            // create the shader program
+            shaderProgram = GL.CreateProgram();
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, LoadShaderSource("Default.vert"));
+            GL.CompileShader(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, LoadShaderSource("Default.frag"));
+            GL.CompileShader(fragmentShader);
+
+            GL.AttachShader(shaderProgram, vertexShader);
+            GL.AttachShader(shaderProgram, fragmentShader);
+
+            GL.LinkProgram(shaderProgram);
+
+            // delete the shaders
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+
+        }
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            GL.DeleteVertexArray(vao);
+            GL.DeleteProgram(shaderProgram);
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0.6f, 0.3f, 1f, 1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+            // draw our triangle
+            GL.UseProgram(shaderProgram);
+            GL.BindVertexArray(vao);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+
+
+            Context.SwapBuffers();
+
+            base.OnRenderFrame(args);
+        }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        // Function to load a text file and return its contents as a string
+        public static string LoadShaderSource(string filePath)
+        {
+            string shaderSource = "";
+
+            try
+            {
+                using (StreamReader reader = new StreamReader("../../../Shaders/" + filePath))
+                {
+                    shaderSource = reader.ReadToEnd();
+                }
+                // Console.WriteLine(shaderSource);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to load shader source file: " + e.Message);
+            }
+
+            return shaderSource;
+        }
+
+    }
+}
